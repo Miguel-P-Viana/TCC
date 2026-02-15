d48245072dc31d1333cce60f33a9d543bfd5f780
unknown <you@example.com>
2026-02-15 12:32:52 -0300

    continuando os modals de login e criar

--- assets/js/pages/lading/modal-criar.js
+++ assets/js/pages/lading/modal-criar.js
@@ -1,10 +1,39 @@
-const abrirmodal = document.querySelector('.abrir-modal')
+const abrirmodalcriar = document.querySelector('.abrir-modal-criar')
+const abrirmodallogar = document.querySelector('.abrir-modal-logar')
+const modalcriar = document.querySelector('.criar-conta')
+const modallog = document.querySelector('.logar-conta')
+const x = document.querySelectorAll('.x')
 
-abrirmodal.addEventListener('click', function() {
 
-    const modalcriar = document.querySelector('.modal-criar-conta')
-    const modallogar = document.querySelector('.modal-logar-conta')
+abrirmodalcriar.onclick = () => {
+    
+    modalcriar.showModal()
+    document.body.style.overflow = 'hidden'
+}
 
-    modalcriar.style.display = "flex"
-    modallogar.style.display = "flex"
+abrirmodallogar.onclick = () => {
+    
+    modallog.showModal()
+    document.body.style.overflow = 'hidden'
+}
+
+
+// forEach junto com querySelectorAll
+
+x.forEach(botaofechar => {
+    botaofechar.onclick = () => {
+
+       
+        const modal = botaofechar.closest('dialog')  // encontra o dialog mais proximo do botao que for clicado
+        modal.close()
+        document.body.style.overflow = 'auto'
+    }
+
 })
+
+    
+    
+    
+modallog.addEventListener('cancel', () => {
+    document.body.style.overflow = 'auto';
+});
